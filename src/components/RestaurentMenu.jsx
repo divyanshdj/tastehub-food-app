a0e418d8a0dfba0db1f647ed5ace89cf0c9a9d35
Divyansh Jain <124493395+divyanshdj@users.noreply.github.com>
2024-05-15 16:36:48 +0530

    CORS API KEY UPDATED

--- src/components/RestaurentMenu.jsx
+++ src/components/RestaurentMenu.jsx
@@ -17,7 +17,7 @@ const RestaurentMenu = () => {
       const corsProxyUrl = "https://proxy.cors.sh/";
       const response = await fetch(corsProxyUrl + MENU_API + resId, {
         headers: {
-          "x-cors-api-key": "temp_2d9a22a9c13bd4fe3b2dede499458cc0",
+          "x-cors-api-key": "temp_5117949ddd5685424a703d0a7310a4ba",
         },
       });
 
